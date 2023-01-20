59a3d1ab0e77599ac4ee090110159d7201e2da23
Carlos Alberto <carlos.neurozze@gmail.com>
2023-01-20 17:51:21 -0300

    Projeto monte seu hamburguer

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main-banner">
+        <h1>Monte Seu Hamburguer!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+
+<style scoped>
+#main-banner {
+    background-image: url('/public/img/burger2.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+#main-banner h1 {
+    color: #FFF;
+    text-align: center;
+    font-size: 60px;
+    border-bottom: 3px solid #DCDCDC;
+    padding: 10px 40px;
+}
+</style>
